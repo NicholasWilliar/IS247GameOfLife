d55b13cad28bbd91c08bf654fde022049e241efe
NicholasWilliar <119467100+NicholasWilliar@users.noreply.github.com>
2022-12-06 19:12:13 -0500

    Update GameView.java

--- GameView.java
+++ GameView.java
@@ -1,93 +1,180 @@
 /**
-* IS 247 Game of Life Project
-* Katarina Nitz & Nicholas Williar
-* View Code
-*/
+ * IS 247 Game of Life Project
+ * Katarina Nitz & Nicholas Williar
+ * View Code
+ */
+import java.util.Scanner;
 
 public class GameView implements GameViewInterface {
-	String heightPrompt = "";
-    	String widthPrompt = "";
-    	String livePrompt = "";
-    	String deadPrompt = "";
-    	String delimiterPrompt = "";
-    	String genPrompt = "";
-    	String sleepPrompt = "";
-    	String patternPrompt = "";
+    String heightPrompt = "";
+    String widthPrompt = "";
+    String livePrompt = "";
+    String deadPrompt = "";
+    String delimiterPrompt = "";
+    String genPrompt = "";
+    String sleepPrompt = "";
+    String patternPrompt = "";
+	char delimiter;
+	int generations;
 
-    	QueryListener queryListener = null;
+    QueryListener queryListener = null;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public GameView() {}
 
-    	// set prompts
-    	public void setHeightPrompt(String h){
-        	this.heightPrompt = h;
-    	}
+    // set prompts
+    public void setHeightPrompt(String h){
+        this.heightPrompt = h;
+    }
 
-    	public void setWidthPrompt(String w){
-        	this.widthPrompt = w;
-    	}
+    public void setWidthPrompt(String w){
+        this.widthPrompt = w;
+    }
 
-    	public void setLivePrompt (String l){
-        	this.livePrompt = l;
-    	}
+    public void setLivePrompt (String l){
+        this.livePrompt = l;
+    }
 
-    	public void setDeadPrompt (String d){
-        	this.deadPrompt = d;
-    	}
+    public void setDeadPrompt (String d){
+        this.deadPrompt = d;
+    }
 
-    	public void setDelimiterPrompt (String del) {
-        	this.delimiterPrompt = del;
-    	}
+    public void setDelimiterPrompt (String del) {
+        this.delimiterPrompt = del;
+    }
+	
+	public void setDelimiter(char del) {
+		this.delimiter = del;
+	}
 
-    	public void setGenPrompt (String g) {
-        	this.genPrompt = g;
-    	}
+    public void setGenPrompt (String g) {
+        this.genPrompt = g;
+    }
+	
+	public void setGenerations(int gen) {
+		this.generations = gen;
+	}
 
-    	public void setSleepPrompt (String s) {
-        	this.sleepPrompt = s;
-    	}
+    public void setSleepPrompt (String s) {
+        this.sleepPrompt = s;
+    }
 
-    	public void setPatternPrompt (String p) {
-        	this.patternPrompt = p;
-    	}
+    public void setPatternPrompt (String p) {
+        this.patternPrompt = p;
+    }
 
-    	public void setListener(QueryListener ql){
-        	this.queryListener = ql;
-    	}
+    public void setListener(QueryListener ql){
+        this.queryListener = ql;
+    }
+	
+	//Get prompts.
+	public String getHeightPrompt() {
+		return heightPrompt;
+	}
+	
+	public String getWidthPrompt() {
+		return widthPrompt;
+	}
+	
+	public String getLivePrompt() {
+		return livePrompt;
+	}
+	
+	public String getDeadPrompt() {
+		return deadPrompt;
+	}
+	
+	public String getDelimiterPrompt() {
+		return delimiterPrompt;
+	}
+	
+	public char getDelimiter() {
+		return delimiter;
+	}
+	
+	public String getGenPrompt() {
+		return genPrompt;
+	}
+	
+	public int getGenerations() {
+		return generations;
+	}
+	
+	public String getSleepPrompt() {
+		return sleepPrompt;
+	}
+	
+	public String getPatternPrompt() {
+		return patternPrompt;
+	}
 
-    	// show/display
-    	public void show() {
-        	// here is where we may need to use getters?
-        	String input = "";
-        	boolean exit = false;
+    // show/display
+    public void show() {
+        int height;
+		int width;
+		char live;
+		char dead;
+		char delimiter;
+		int gen;
+		int sleep;
+		String pattern;
+        String input = "";
+        boolean exit = false;
 
-        	while (!exit) {
-            	// and here?
-            		System.out.println("Play again? Y/N");
-            		if (input.equalsIgnoreCase("y"));
-                		exit = true;
-
-        	} // while
-    	} // show()
+       // while (!exit) {
+            System.out.println(getHeightPrompt());
+			height = sc.nextInt();
+			queryListener.heightEntered(height);
+			
+			System.out.println(getWidthPrompt());
+			width = sc.nextInt();
+			queryListener.widthEntered(width);
+			
+			System.out.println(getLivePrompt());
+			live = sc.next().charAt(0);
+			queryListener.liveEntered(live);
+			
+			System.out.println(getDeadPrompt());
+			dead = sc.next().charAt(0);
+			queryListener.deadEntered(dead);
+			
+			System.out.println(getDelimiterPrompt());
+			delimiter = sc.next().charAt(0);
+			
+			System.out.println(getGenPrompt());
+			gen = sc.nextInt();
+			queryListener.genEntered(gen);
+			
+			System.out.println(getSleepPrompt());
+			sleep = sc.nextInt();
+			queryListener.sleepEntered(sleep);
+			
+			System.out.println(getPatternPrompt());
+			pattern = sc.next();
+			queryListener.patternEntered(pattern);
+			
+          /*  System.out.println("Play again? (Y/N)");
+            if (input.equalsIgnoreCase("n")) {
+				exit = true;
+				queryListener.quit();
+			} //if
+        } // while*/
+    } // show()
 	/* the example mvc number problem uses getters here and the listener
 	but i'm not sure that's necessary? */
 
-    	public void displayGen(char[][] g) {
-        	for(int row = 0; row < g.length; row++) {
-            		for(int column = 0; column < g[row].length; column++) {
-                		System.out.print(g[row][column] + " ");
-            		} // columns
-       		} // rows
-    	} // displayGen
-} // end	
-
-
-
-		/*Reference
-		
-		//Printing current gen array.
-		for(int row = 0; row < gen1.length; row++) {
-			for(int column = 0; column < gen1[row].length; column++) {
-				System.out.print(gen1[row][column] + " ");
-			}
+    public void displayGen(char[][] g) {
+		System.out.println("Game of Life - Generation: " + 2);
+        for(int row = 0; row < g.length; row++) {
+            for(int column = 0; column < g[row].length; column++) {
+                System.out.print(g[row][column] + "  ");
+            } // columns
 			System.out.println();
+        } // rows
+		//Print delimiter.
+		for() {
+			System.out.print(delimiter);
 		}
-		*/
+    } // displayGen
+} // end	
