c924def497b9637e6d725c2f6169f8260a5765e3
NicholasWilliar <119467100+NicholasWilliar@users.noreply.github.com>
2022-12-06 22:02:33 -0500

    Update QueryListener.java

--- QueryListener.java
+++ QueryListener.java
@@ -16,52 +16,13 @@ public interface QueryListener {
 	
 	//Dead cell character has been entered.
 	void deadEntered(char d);
-	
+
 	//Generations has been entered.
 	void genEntered(int g);
-	
+
 	//Time between generations has been entered.
 	void sleepEntered(int s);
 	
 	//Pattern file name has been entered.
 	void patternEntered(String p);
 }
-
-
-
-
-
-
-
-
-
-
-/** REFERENCE
- * file: NumberListener.java
- *
- * LISTING 4
- *
- * NumberListener is implemented by the MVC controller
- *
- * Any class that wants to use a view in the framework implements
- * NumberListener. This allows the view to be plug and play with
- * other classes.
- *
- * The actual view, whether text based or GUI based, will contain
- * an object that implements NumberListener. In the MVC architecture,
- * the model and view do not communicate directly. Therefore, the
- * controller that wants to use a view, must implement
- * NumberListener.
- *
- * This architecture allows the instance in the view class
- * to be based on an interface rather than a specific class
- 
-
-public interface NumberListener {
-     // number has been entered 
-     void numberEntered(int n);
-
-     // user wants to quit 
-     void quit();
-}
-*/
